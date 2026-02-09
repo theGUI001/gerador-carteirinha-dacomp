b85fcb7b38ba0d27db422f47c5af500caceb3f06
theGUI001 <lghmelo@gmail.com>
2026-02-09 18:03:49 +0000

    feat: add Next.js App Router structure

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Navbar } from '@/components/Navbar'
+
+export const metadata = {
+    title: 'Gerador de Carteirinhas DACOMP',
+    description: 'Gere carteirinhas de estudantes da UTFPR',
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-BR">
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+            </head>
+            <body>
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    )
+}
